f77de1003787876c071d0d40f0a8a6c38b7ce8fa
Chen Wilson <chenlwilson@gmail.com>
2019-01-20 18:44:18 -0800

    did a bit of front-end reorg and styling

---
+++ client/src/components/UserProfile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const UserProfile = () => (
+  <div>
+    <h4>
+      <img style={{width:'100px',height:'100px'}} src='https://avatars0.githubusercontent.com/u/583231?v=3'></img>
+      <br/>
+      Name: Octocat
+      </h4>
+    <ul>
+    <li><a href='http://www.github.com'>repositories</a></li>
+    <li><a href='http://www.github.com'>orginazations</a></li>
+    </ul>
+  </div>
+)
+
+export default UserProfile;
