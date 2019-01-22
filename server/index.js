1b2e0d39ac703fd4cab7885e1fd2a7355764ffd9
Chen Wilson <chenlwilson@gmail.com>
2019-01-22 14:51:48 -0800

    still working on heroku connection

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.listen(port, function() {
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.text());
 
-app.post('/', function (req, res) {
+app.post('/repos', function (req, res) {
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
@@ -52,7 +52,7 @@ app.post('/', function (req, res) {
 
 });
 
-app.get('/', function (req, res) {
+app.get('/repos', function (req, res) {
   // This route should send back the top 25 repos
   getTopRepos((err, results) => {
     if (err) {
