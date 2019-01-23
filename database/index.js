412aea759f3838563d8eec132a75501ac9f55182
Chen Wilson <chenlwilson@gmail.com>
2019-01-22 21:27:06 -0800

    fixing heroku

--- database/index.js
+++ database/index.js
@@ -5,7 +5,8 @@ var config = require('dotenv').config();
 ///////////////////////Schema//////////////////////
 var Schema = mongoose.Schema;
 //var local = 'mongodb://localhost:27017/fetcher'
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true });
+var MONGODB_URI = 'mongodb://heroku_jvbvm8h6:f9fet4b0psbhg32rq61gmfqn4l@ds147411.mlab.com:47411/heroku_jvbvm8h6'
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
