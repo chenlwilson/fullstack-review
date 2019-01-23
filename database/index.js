d97d3a5be7a6742c43e3656239b2b4e1e40fbe45
Chen Wilson <chenlwilson@gmail.com>
2019-01-22 21:43:22 -0800

    try config var again

--- database/index.js
+++ database/index.js
@@ -5,8 +5,8 @@ var config = require('dotenv').config();
 ///////////////////////Schema//////////////////////
 var Schema = mongoose.Schema;
 //var local = 'mongodb://localhost:27017/fetcher'
-var MONGODB_URI = 'mongodb://heroku_jvbvm8h6:f9fet4b0psbhg32rq61gmfqn4l@ds147411.mlab.com:47411/heroku_jvbvm8h6'
-mongoose.connect(process.env.URI), { useNewUrlParser: true });
+//var MONGODB_URI = 'mongodb://heroku_jvbvm8h6:f9fet4b0psbhg32rq61gmfqn4l@ds147411.mlab.com:47411/heroku_jvbvm8h6'
+mongoose.connect(MONGODB_URI), { useNewUrlParser: true });
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
